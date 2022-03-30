1deb7cc77fe3cd6a0e3063176349417342ade585
Janet Barclay <jbarclay@usgs.gov>
2022-03-30 15:50:42 -0400

    switch url get method

--- gw_stream_temp/getGWModelArchive.py
+++ gw_stream_temp/getGWModelArchive.py
@@ -3,5 +3,5 @@ import urllib.request
 def get_gw_archive_data(archiveCode, basefile, destination):
     #make the url
     url = "https://water.usgs.gov/GIS/dsdl/gwmodels/{}/{}".format(archiveCode,basefile)
-    urllib.request.urlretrieve(url, destination)
+    wget.download(url, destination)
     
